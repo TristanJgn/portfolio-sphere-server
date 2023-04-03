d0cf3fd06f363b267118e0784fdda280a0ab07d3
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-03 00:07:35 -0400

    adds changes from hr to h for table column names and seed data keys

--- migrations/20230403033659_create_coin_data_table.js
+++ migrations/20230403033659_create_coin_data_table.js
@@ -5,11 +5,11 @@ exports.up = function (knex) {
       table.string("name").notNullable();
       table.string("symbol").notNullable();
       table.decimal("price", 14, 2).notNullable(); // Allows for price to be 14 numbers with 2 decimals for the cents
-      table.decimal("percent_change_1hr", 14, 4).notNullable(); // Percentage to be displayed with 2 decimal points when multiplied by 100 so must allow 4 decimals
-      table.decimal("percent_change_24hr", 14, 4).notNullable(); // Percentage to be displayed with 2 decimal points when multiplied by 100 so must allow 4 decimals
+      table.decimal("percent_change_1h", 14, 4).notNullable(); // Percentage to be displayed with 2 decimal points when multiplied by 100 so must allow 4 decimals
+      table.decimal("percent_change_24h", 14, 4).notNullable(); // Percentage to be displayed with 2 decimal points when multiplied by 100 so must allow 4 decimals
       table.decimal("percent_change_7d", 14, 4).notNullable(); // Percentage to be displayed with 2 decimal points when multiplied by 100 so must allow 4 decimals
       table.decimal("market_cap", 14, 0).notNullable(); // Use decminal as values exceed the integer limit
-      table.decimal("volume_24hr", 14, 0).notNullable(); // Use decminal as values exceed the integer limit
+      table.decimal("volume_24h", 14, 0).notNullable(); // Use decminal as values exceed the integer limit
       table.timestamp("updated_at").defaultTo(knex.fn.now());
     });
 };
