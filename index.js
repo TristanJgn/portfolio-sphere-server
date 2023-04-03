019fc4a8a876dfec59c2363d66114e554c80109e
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-02 23:50:58 -0400

    adds use of routes in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const coinsRoutes = require("./routes/coinsRoute");
 
 // Create an instance of express
 const app = express();
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 8000;
 // Add middleware
 app.use(cors());
 app.use(express.json());
+
+// Define routes
+app.use("/coins", coinsRoutes);
 
 // Bootstrap server
 app.listen(PORT, () => {
