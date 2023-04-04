80cd4d630ba832bcfbfce43c37f39e838497cc27
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-04 12:51:27 -0400

    adds a GET request to the portfolio route which authorizes the user and then returns their holdings

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,3 +1,20 @@
 const knex = require("knex")(require("../knexfile"));
 
-exports.index = (req, res) => {}
+exports.index = (req, res) => {
+    knex("user_holdings")
+      .select(
+        "coin_id",
+        "coin_name",
+        "coin_amount",
+      )
+      .where({ user_id: req.userId })
+      .then((holdings) => {
+        return res.json(holdings);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "There was an issue with the request",
+          err,
+        });
+      });
+};
--- routes/portfolioRoute.js
+++ routes/portfolioRoute.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const portfolioController = require("../controllers/portfolioController");
+const authorize = require("../middleware/authorize");
 
-router.route("/").get(portfolioController.index);
+router.route("/").get(authorize, portfolioController.index);
 
 module.exports = router;
