d845f35f46d8075880f89a4850ce1c18dfb2ef76
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-05 12:59:50 -0400

    adds use of async/await structure for add coin controller function to avoid issue of returning multiple items that the .then structure was running into

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -20,53 +20,54 @@ exports.index = (req, res) => {
       });
 };
 
-exports.addCoin = (req, res) => {
-  const { coin_id, coin_name, coin_symbol, coin_amount} = req.body;
+exports.addCoin = async (req, res) => {
+  try {
+    const { coin_id, coin_name, coin_symbol, coin_amount} = req.body;
 
-  if (!coin_amount) {
-    return res.status(400).json({message: "Amount is required"});
-  }
+    // Check that the amount trying to be entered is greater than 0
+    if (coin_amount <= 0) {
+      return res.status(400).json({message: "Amount is required"});
+    }
 
-  knex("user_holdings")
-    .where({ user_id: req.userId, coin_id: coin_id })
-    .then((foundCoin) => {
-      if (foundCoin.length !== 0) {
-        return res.status(400).json({
-            message: "Coin is already in portfolio",
-        });
-      }
+    // Check that the user doesn't already have the coin in their portfolio
+    const foundCoin = await knex("user_holdings").where({ user_id: req.userId, coin_id: coin_id })
 
-      return knex("user_holdings").insert({
-        user_id: req.userId,
-        coin_id,
-        coin_name,
-        coin_symbol,
-        coin_amount,
+    if (foundCoin.length !== 0) {
+      return res.status(400).json({
+          message: "Coin is already in portfolio",
       });
-    })
-    .then(() => {
-        return knex("user_holdings").where({
-        user_id: req.userId,
-        coin_id: coin_id,
-        })
-        .select(
-        "coin_id",
-        "coin_name",
-        "coin_symbol",
-        "coin_amount",
-        );
-    })
-    .then((newCoin) => {
-        return res.status(201).json(newCoin[0]);
-    })
-    .catch((err) => {
-      return res.status(500).json({
-        message: "There was an issue with the request",
-        err,
-      });
+    }
+
+    // If it is a new coin, then insert it into the user holdings table
+    await knex("user_holdings").insert({
+      user_id: req.userId,
+      coin_id,
+      coin_name,
+      coin_symbol,
+      coin_amount,
     });
-};
 
+    // Grab the newly added coin and return it with a 201 status to let the user know it has been successfully created
+    const newCoin = await knex("user_holdings").where({
+    user_id: req.userId,
+    coin_id: coin_id,
+    })
+    .select(
+    "coin_id",
+    "coin_name",
+    "coin_symbol",
+    "coin_amount",
+    );
+
+    res.status(201).json(newCoin[0]);
+  }
+  catch (err) {
+    return res.status(500).json({
+      message: "There was an issue with the request",
+      err,
+    });
+  }
+};
 
 exports.deleteCoin = (req, res) => {
   knex("user_holdings")
