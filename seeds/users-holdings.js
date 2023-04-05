6de9680d722f1d317f4f0a1674a7d8792129199d
Tristan Jagan <tristan.jgn@gmail.com>
2023-04-05 01:18:34 -0400

    adds updated seed data

--- seeds/users-holdings.js
+++ seeds/users-holdings.js
@@ -32,7 +32,7 @@ const userHoldingsData = [
   },
   {
     user_id: 1,
-    coin_id: 2,
+    coin_id: 1027,
     coin_name: "Ethereum",
     coin_symbol: "ETH",
     coin_amount: 2,
@@ -46,7 +46,7 @@ const userHoldingsData = [
   },
   {
     user_id: 2,
-    coin_id: 2,
+    coin_id: 1027,
     coin_name: "Ethereum",
     coin_symbol: "ETH",
     coin_amount: 5,
